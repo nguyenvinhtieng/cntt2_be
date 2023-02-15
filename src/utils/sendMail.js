47ce8e59119fb06dd10130c540c1491189bdc1f1
Vinh.Tieng <vinh.tieng@laichi.vn>
2023-02-15 13:27:02 +0700

    change email css

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -14,9 +14,17 @@ async function sendMail(receiver, otp) {
             rejectUnauthorized: false,
         },
     });
-    let contentHTML = "Vui lòng truy cập vào link sau để đổi mật khẩu: </br>" + url+otp;
+    let contentHTML = `<div style="max-width: 600px; margin-left: auto; margin-right: auto; background: #fff; border-radius: 8px; overflow: hidden;font-family: Verdana, Geneva, Tahoma, sans-serif;">
+    <h2 style="padding: 25px; background-color: rgb(59, 115, 220); color: #fff; text-align: center;margin: 0;">Yêu cầu đặt lại mật khẩu</h2>
+    <div style="">
+      <p>Bạn vừa yêu cầu đổi mật khẩu từ CNTT2</p>
+      <p>Để đặt lại mật khẩu, vui lòng nhấn vào nút bên dưới</p>
+      <a href="${url + otp}" style="display: inline-block; background-color: rgb(59, 115, 220); color: #fff; padding: 10px 20px; border-radius: 5px; text-decoration: none; margin: 10px auto;" target="_blank">Đặt lại mật khẩu</a>
+      <p>Nếu bạn không yêu cầu đặt lại mật khẩu, vui lòng bỏ qua email này</p>
+    </div>
+  </div>`;
     let subject = "Yêu cầu đặt lại mật khẩu";
-    let content = `Send Email By Nodemailer`;
+    let content = `CNTT 2`;
     let mainOptions = {
         from: "System",
         to: receiver,
